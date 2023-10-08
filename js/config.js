00e830feecd12971a6385ffcaec62c5b413b7a08
Dominique Caillard <dominiquecaillard@MacBookominique.lan>
2023-10-08 16:07:59 +0200

    new greatconfig

--- js/config.js
+++ js/config.js
@@ -1,10 +1,10 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-west-1_qajhfvfMC
+        userPoolId: '', // e.g. ue-west-1_qajhfvfMC
         userPoolClientId: '', // e.g. 8gj90q2dkbld65n3p1cfvnic0
-        region: '' // e.g. us-west-1
+        region: '' // e.g. ue-west-1
     },
     api: {
-        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.ue-west-1.amazonaws.com/prod',
     }
 };
